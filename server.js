be693d98c95824bbfda71c344fd63af85504cade
Arunabh BH <arunabh2023@gmail.com>
2025-09-04 16:39:38 +0530

    completed edit and delete feature

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const userModel = require("./models/user-model");
-const todomodel = require("./models/todo-model");
+const todoModel = require("./models/todo-model");
 app.use(express.json()); // To parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // To parse form bodies
 
@@ -20,6 +20,15 @@ const connectDB = async () => {
   }
 };
 connectDB();
+app.get("/", async (req, res) => {
+  try {
+    const todos = await todoModel.find({});
+    res.render("home", { todos: todos }); // Pass the todos array to the EJS file
+  } catch (error) {
+    console.error("Error fetching todos:", error);
+    res.status(500).send("Server error.");
+  }
+});
 
 app.get("/register", (req, res) => {
   res.render("register");
@@ -42,14 +51,44 @@ app.post("/login", async (req, res) => {
   let user = await userModel.findOne({ email });
   if (!user) {
     return res.send("There is no such user");
-  } else return res.send(user);
+  } else return res.redirect("/");
 });
-app.post("/create new todo", async (req, res) => {
+app.post("/create", async (req, res) => {
   let { title, description } = req.body;
-  let todo = await todo.create({
+  let todo = await todoModel.create({
     title,
     description,
   });
+  res.redirect("/");
+});
+app.post("/delete/:id", async (req, res) => {
+  let todoId = req.params.id;
+  await todoModel.findByIdAndDelete(todoId);
+  res.redirect("/");
+});
+app.get("/edit/:id", async (req, res) => {
+  try {
+    const todoId = req.params.id;
+    const todo = await todoModel.findById(todoId);
+    if (!todo) {
+      return res.send("Todo not found.");
+    }
+    res.render("edit", { todo: todo });
+  } catch (error) {
+    console.error("Error fetching todo for edit:", error);
+    res.send("Server error.");
+  }
+});
+
+app.post("/edit/:id", async (req, res) => {
+  try {
+    const todoId = req.params.id;
+    const { title, description } = req.body; 
+    await todoModel.findByIdAndUpdate(todoId, { title, description });
+    res.redirect("/");
+  } catch (error) {
+    res.send("Server error." + error);
+  }
 });
 
 app.listen(5000);
